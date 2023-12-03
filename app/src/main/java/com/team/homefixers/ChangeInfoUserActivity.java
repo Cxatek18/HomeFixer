e4006bbc1f3509cc85955268387d0e2c48502100
Cxatek18 <griga2888@mail.ru>
2023-12-03 13:06:24 +0500

    Added activity modification of profile information, implemented the layout of this screen.

---
+++ app/src/main/java/com/team/homefixers/ChangeInfoUserActivity.java
@@ -0,0 +1,14 @@
+package com.team.homefixers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeInfoUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_info_user);
+    }
+}
